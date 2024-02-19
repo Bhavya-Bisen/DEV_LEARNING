0ee40ee59fbb318fafe7754051e502a893afe1ce
Bhavya Bisen <81218805+Bhavya-Bisen@users.noreply.github.com>
2024-02-19 19:01:55 +0530

    Updaate adding a new project 'Dice Challenge' and DOM challenge last update

--- DOM Challenge Starting Files/index.js
+++ DOM Challenge Starting Files/index.js
@@ -1,3 +1,4 @@
 document.query;
 document.firstElementChild.lastElementChild.firstElementChild.innerHTML="Bhavya";
 document.querySelector("li a").style.color="red";
+document.querySelector("button").style.backgroundColor="yellow";
